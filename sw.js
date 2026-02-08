65a625648c2b56bdecf6379e92b812c7bba6c73d
ProjectvakAKA <afv2@illinois;edu>
2026-02-08 14:53:11 +0100

    derde

---
+++ sw.js
@@ -0,0 +1,45 @@
+// FlowChat Service Worker
+const CACHE_NAME = 'flowchat-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  // Skip API calls, only cache static assets
+  if (event.request.url.includes('/api/')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
